f03fc3d297f8704c57592e3a61771990ffb11c58
dave <davidpascual200@gmail.com>
2022-05-07 18:14:45 -0700

    added saved storage and loop

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Work-Day-Scheduler
+
+## Summary
+
+this project consists of workday time blocks that allows the user to input information inside and be able to save the inputted information onto local storage. 
+
+the work hours of the day are displated on the right side of the input box.
+
+the "save" button is displayed on the left hand side of the input box. 
+
+this project uses a combination of:
+
+* jQuery
+* Bootstrap
+* javaScript
+
+## Deployed page
+https://davidpascual2.github.io/Work-Day-Scheduler/
+
+## Repository Link
+https://github.com/davidpascual2/Work-Day-Scheduler
+
+
+## Preview
+![](./assetes/workday-chedule-SS.png)
--- assetes/script.js
+++ assetes/script.js
@@ -4,75 +4,48 @@ $("#timer").text(currentDate);
 
 // define save button
 var saveButtonEl = $(".btn")
+var currentHour = moment().hours();
+let userText = $(".row textarea");
 
-//savebutton click listener
-saveButtonEl.on('click', function () {
-    console.log("you clicked the button")
-// get row text feild class and row id values
-    
-var value= $(this).siblings(".description").val()
-var time = $(this).parent().attr("id");
-    console.log(this, "value", currentDate, "current Date")
+  // function
+  $(userText).each(function() {
 
+    // var blockTime = $(this).data(".hour");
+    let hourValue = $(this).data("hour");
 
-// save in local storage
-    localStorage.setItem(currentDate,value);
-});
-
-    
-function hourTracker() {
-  // get current number of hours
-  var currentHour = moment().hours();
-
-  // loop over time blocks
-  $(".row").each(function() {
-    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-    // check if we've moved past this time
-    if (blockTime < currentHour) {
-        $(this).removeClass("future");
-        $(this).removeClass("present");
+    if (hourValue < currentHour) {
         $(this).addClass("past");
-    } else if (blockTime === currrentHour) {
-        $(this).removeClass("future");
+    } else if (hourValue === currentHour) {
         $(this).addClass("present");
-        $(this).removeClass("past");
     } else {
         $(this).addClass("future");
-        $(this).removeClass("present");
-        $(this).removeClass("past");
     }
     
-
-  // set up interval to check if current time needs to be updated
-
-  // load any saved data from localStorage
   });
-}
+
+// savebutton click listener
+saveButtonEl.on('click', function () {
+    console.log("you clicked the button")
+
+    var eventEntered= $(this).siblings(".description").val()
+    var eventHour = $(this).parent().attr("id");
+    // var eventEntered =$(this).siblings(".description").val(); // explain
+    // var eventHour = $(this).data("id"); // explain
+    
+
+    console.log(eventHour);
+    localStorage.setItem(eventHour, eventEntered);
+
+});
+
+//create function that will keep data on page after refresh
+$(userText).each(function(){
+    var hour = $(this).data("hour");
+    
+    var description = localStorage.getItem(hour);
+    $(this).val(description);
+});
 
 
-// const currentHour = moment().hours();
-// let usetText = $(".row description");
 
-
-
-
-  // check if we've moved past this time
-
-  // set up interval to check if current time needs to be updated
-
-  // load any saved data from localStorage
-
-
-
-  // $("#hour8 .description").val(localStorage.getItem("hour8"));
-//     $("#hour9 .description").val(localStorage.getItem("hour9"));
-//     $("#hour10 .description").val(localStorage.getItem("hour10"));
-//     $("#hour11 .description").val(localStorage.getItem("hour11"));
-//     $("#hour12 .description").val(localStorage.getItem("hour12"));
-//     $("#hour13 .description").val(localStorage.getItem("hour13"));
-//     $("#hour14 .description").val(localStorage.getItem("hour14"));
-//     $("#hour15 .description").val(localStorage.getItem("hour15"));
-//     $("#hour16 .description").val(localStorage.getItem("hour16"));
-//     $("#hour17 .description").val(localStorage.getItem("hour17"));
-
+    
